f99756e7c0832b533288d0b0d5527b9ec4594193
Kiptoy <88104909+Kiptoy@users.noreply.github.com>
2022-08-29 17:29:20 +0700

    Fix v1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,6 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  return word.startsWith(startWord) ? word : startWord + word
+  return word.startsWith(startWord) ? word : (word == null || word == undefined) ? undefined : startWord + word
 }
 module.exports = fillStartWord
